16e54ce10eb1b7d1ff599246989fa67c34276ef0
seongahong <girl0930h@gmail.com>
2024-10-06 14:54:45 +0900

    [add] explore페이지 퍼블리싱

--- src/app/(afterLogin)/[username]/status/[id]/page.tsx
+++ src/app/(afterLogin)/[username]/status/[id]/page.tsx
@@ -1,3 +1,3 @@
-export default function page() {
-  return <div>page</div>;
+export default function UserStatusPage() {
+  return <div>UserStatusPage</div>;
 }
--- src/app/(afterLogin)/explore/page.tsx
+++ src/app/(afterLogin)/explore/page.tsx
@@ -1,3 +1,29 @@
+import SearchForm from "../_component/SearchForm";
+import Trend from "../_component/Trend";
+import style from "./explore.module.css";
+
 export default function ExplorePage() {
-  return <div>ExplorePage</div>;
+  return (
+    <>
+      <main className={style.main}>
+        <div className={style.formZone}>
+          <SearchForm />
+        </div>
+        <div className={style.trend}>
+          <h3>Trends for you</h3>
+          <Trend />
+          <Trend />
+          <Trend />
+          <Trend />
+          <Trend />
+          <Trend />
+          <Trend />
+          <Trend />
+          <Trend />
+          <Trend />
+          <Trend />
+        </div>
+      </main>
+    </>
+  );
 }
